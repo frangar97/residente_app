3e44b3e4e7e5b83f8e7cc20503225f1412491039
Francisco Garcia <frangarcia38@hotmail.com>
2022-06-04 08:05:34 -0600

    Funcionalidad para crear comunicados

--- lib/cubits/comunicado/comunicado_state.dart
+++ lib/cubits/comunicado/comunicado_state.dart
@@ -4,23 +4,48 @@ class ComunicadoState extends Equatable {
   final bool loadingComunicados;
   final List<ComunicadoModel> listaComunicados;
   final ComunicadoModel? comunicadoSelected;
+  final String titulo;
+  final String descripcion;
+  final String fecha;
+  final FormSubmissionStatus formStatus;
 
   const ComunicadoState(
       {this.loadingComunicados = false,
       this.listaComunicados = const [],
-      this.comunicadoSelected});
+      this.comunicadoSelected,
+      this.titulo = "",
+      this.descripcion = "",
+      this.fecha = "",
+      this.formStatus = const InitialFormStatus()});
 
-  ComunicadoState copyWith(
-      {bool? loadingComunicados,
-      List<ComunicadoModel>? listaComunicados,
-      ComunicadoModel? comunicadoSelected}) {
+  ComunicadoState copyWith({
+    bool? loadingComunicados,
+    List<ComunicadoModel>? listaComunicados,
+    ComunicadoModel? comunicadoSelected,
+    String? titulo,
+    String? descripcion,
+    String? fecha,
+    FormSubmissionStatus? formStatus,
+  }) {
     return ComunicadoState(
-        comunicadoSelected: comunicadoSelected ?? this.comunicadoSelected,
-        loadingComunicados: loadingComunicados ?? this.loadingComunicados,
-        listaComunicados: listaComunicados ?? this.listaComunicados);
+      comunicadoSelected: comunicadoSelected ?? this.comunicadoSelected,
+      loadingComunicados: loadingComunicados ?? this.loadingComunicados,
+      listaComunicados: listaComunicados ?? this.listaComunicados,
+      titulo: titulo ?? this.titulo,
+      descripcion: descripcion ?? this.descripcion,
+      fecha: fecha ?? this.fecha,
+      formStatus: formStatus ?? this.formStatus,
+    );
   }
 
   @override
-  List<Object?> get props =>
-      [loadingComunicados, listaComunicados, comunicadoSelected];
+  List<Object?> get props => [
+        loadingComunicados,
+        listaComunicados,
+        comunicadoSelected,
+        titulo,
+        descripcion,
+        fecha,
+        formStatus
+      ];
 }
